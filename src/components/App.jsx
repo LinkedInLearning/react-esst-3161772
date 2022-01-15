f3c7e4c129995bf41851a2e67c84317fe3a5f891
David Lorenz <info@activenode.de>
2022-01-15 11:49:47 +0100

    setState countdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 export class App extends React.Component {
+  state = {
+    counterValue: 3,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((state) => {
+        if (state.counterValue > 0) {
+          return { counterValue: state.counterValue - 1 };
+        } else {
+          return { counterValue: 0 };
+        }
+      });
+    }, 1000);
+  }
+
   render() {
-    return "Lifecycle Methods";
+    return <strong>Countdown = {this.state.counterValue}</strong>;
   }
 }
