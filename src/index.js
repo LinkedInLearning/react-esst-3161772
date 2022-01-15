02776f6082d7d35d1b3792f3ded875554ddd83c9
David Lorenz <info@activenode.de>
2022-01-15 11:26:36 +0100

    rem nameObj

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
 
-const nameObj = {
-  prename: "David",
-  lastname: "Lorenz",
-};
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
