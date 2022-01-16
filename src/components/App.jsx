f1a501d280a7a05680c4047d95a99b113873c602
David Lorenz <info@activenode.de>
2022-01-16 11:19:07 +0100

    button counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function App() {
-  return <p>An apple a day keeps the doctor away</p>;
+  const [counterState, setCounterState] = useState(0);
+  const [counterState2, setCounterState2] = useState(0);
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          setCounterState(counterState + 1);
+          setCounterState2(counterState2 + 2);
+        }}
+      >
+        Click me
+      </button>
+      CounterValue = {counterState}, CounterValue2 = {counterState2}
+    </div>
+  );
 }
