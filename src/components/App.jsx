40df8c629f4bce920481e7baa7474f73074ff43a
David Lorenz <info@activenode.de>
2022-01-16 14:25:22 +0100

    useMemo and useCallback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 export function App() {
-  const [countdown, setCountdown] = useState(10);
+  const [countdown, setCountdown] = useState(5);
+  const timeoutFunc = useCallback(() => {
+    if (countdown > 0) {
+      setCountdown(countdown - 1);
+    }
+  }, [countdown]);
+
   useEffect(() => {
-    const ref = setTimeout(() => {
-      if (countdown > 0) {
-        setCountdown(countdown - 1);
-      }
-    }, 1000);
+    const ref = setTimeout(timeoutFunc, 1000);
 
     return () => clearTimeout(ref);
   }, [countdown]);
 
-  return (
-    <div>
-      <h1>Countdown</h1>
-      <p>{countdown}</p>
-      <progress value={countdown} min="0" max="10" />
-    </div>
-  );
+  const markup = useMemo(() => {
+    return (
+      <div>
+        <h1>Countdown</h1>
+        <p>{countdown}</p>
+        <progress value={countdown} min="0" max="5" />
+      </div>
+    );
+  }, [countdown]); // componentShouldUpdate (if countdown...)
+
+  return markup;
 }
