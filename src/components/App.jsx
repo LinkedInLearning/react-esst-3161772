b64a39375e0d66eabce78a270ebac7197abf1daf
David Lorenz <info@activenode.de>
2022-01-17 18:26:57 +0100

    user interaction sample added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function App() {
+  const [headline, setHeadline] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const buttonClicked = () => {
+    setHeadline(inputValue);
+  };
+
+  const inputChanged = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div>
-      <button type="button">Click me</button>
+      <h1>{headline}</h1>
+      <input type="text" onInput={inputChanged} />
       <br />
-      <input type="text" />
+      <button type="button" onClick={buttonClicked}>
+        Make me big!
+      </button>
     </div>
   );
 }
