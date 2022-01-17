da6a1ca34f6508d67795dca6491e906331375838
David Lorenz <info@activenode.de>
2022-01-17 16:31:00 +0100

    base setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-import { Course } from "./Course";
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.headline}</h1>
-
-        <Course
-          title="Nebenprojekte als Softwareentwickler:in nutzen"
-          author="Kai Klostermann"
-        />
-
-        <Course title="React Hooks Grundkurs" author="David Lorenz" />
-
-        <Course author="Happy Cat" />
-      </div>
-    );
-  }
+export function App() {
+  return (
+    <div>
+      <button type="button">Click me</button>
+      <br />
+      <input type="text" />
+    </div>
+  );
 }
-
-App.defaultProps = {
-  headline: "Meine coolen Kurse",
-};
-
-App.displayName = "Courses";
