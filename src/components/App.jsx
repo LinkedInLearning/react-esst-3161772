3e3bcf6c26dda622675726622cdea4236ea5c58a
David Lorenz <info@activenode.de>
2022-01-17 09:00:21 +0100

    base setup ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,13 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React from "react";
 
-export function App() {
-  const [countdown, setCountdown] = useState(5);
-  const timeoutFunc = useCallback(() => {
-    if (countdown > 0) {
-      setCountdown(countdown - 1);
-    }
-  }, [countdown]);
-
-  useEffect(() => {
-    const ref = setTimeout(timeoutFunc, 1000);
-
-    return () => clearTimeout(ref);
-  }, [timeoutFunc]);
-
-  const markup = useMemo(() => {
+export class App extends React.Component() {
+  render() {
     return (
       <div>
-        <h1>Countdown</h1>
-        <p>{countdown}</p>
-        <progress value={countdown} min="0" max="5" />
+        <h1>Meine Kurse</h1>
+
+        <p>Hier sollen meine Kurse stehen</p>
       </div>
     );
-  }, [countdown]); // componentShouldUpdate (if countdown...)
-
-  return markup;
+  }
 }
