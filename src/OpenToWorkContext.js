b25abf631613291c136bd445a1cfe25bfc692164
David Lorenz <info@activenode.de>
2022-01-18 19:25:06 +0100

    update

--- src/OpenToWorkContext.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-export const OpenToWorkContext =
-  React.createContext(false);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,9 @@
 import React from "react";
-import { OpenToWorkContext } from "../OpenToWorkContext";
-import { BusinessProfile } from "./BusinessProfile";
 
 export function App() {
   return (
     <div>
-      <h1>Contexts &amp; Theming</h1>
-
-      <BusinessProfile
-        name="David Lorenz"
-        job="Frontend Architect"
-      />
-
-      <OpenToWorkContext.Provider value={true}>
-        <div>
-          <h2>Open to work:</h2>
-
-          <BusinessProfile
-            name="Maus"
-            job="Käse essen"
-          />
-
-          <BusinessProfile
-            name="Pflanze"
-            job="Luft aufbereiten"
-          />
-        </div>
-      </OpenToWorkContext.Provider>
+      <h1>Global State Context</h1>
     </div>
   );
 }
