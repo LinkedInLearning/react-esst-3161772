8889fca21a00861fb10b237462d2d3b19a3db80d
David Lorenz <info@activenode.de>
2022-01-18 16:17:12 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,9 @@
 import React from "react";
-import { Linkchecker } from "./Linkchecker";
 
 export function App() {
   return (
     <div>
-      <h1>Linkchecker</h1>
-
-      <Linkchecker />
-
-      <ul>
-        <li>
-          <a href="https://linkedin.com">LinkedIn</a>
-        </li>
-        <li>
-          <a href="https://blog.activenode.de">David's Blog</a>
-        </li>
-        <li>
-          <a href="https://unsecure-link">Bank Website</a>
-        </li>
-      </ul>
+      <h1>Contexts &amp; Theming</h1>
     </div>
   );
 }
