d9071910c99b1c2db2743ae304c54303d75da7ba
David Lorenz <info@activenode.de>
2022-01-18 20:21:27 +0100

    scaled down sample

--- src/WeatherContext.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const WeatherContext = React.createContext();
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,46 +3,15 @@ import { WeatherContext } from "../WeatherContext";
 import { Weather } from "./Weather";
 
 export function App() {
-  const [weather, setWeather] = useState({});
-
-  useEffect(() => {
-    setTimeout(() => {
-      setWeather((prevWeatherObject) => {
-        return {
-          ...prevWeatherObject,
-          Berlin: "rainy",
-        };
-      });
-    }, 500);
-
-    setTimeout(() => {
-      setWeather((prevWeatherObject) => {
-        return {
-          ...prevWeatherObject,
-          London: "sunny",
-        };
-      });
-    }, 1000);
-
-    setTimeout(() => {
-      setWeather((prevWeatherObject) => {
-        return {
-          ...prevWeatherObject,
-          Stuttgart: "hot and cold",
-        };
-      });
-    }, 1500);
-  }, []);
-
   return (
     <div>
-      <h1>🌥 Weather:</h1>
-
-      <WeatherContext.Provider value={weather}>
-        <Weather city="Stuttgart" />
-        <Weather city="London" />
-        <Weather city="Berlin" />
-      </WeatherContext.Provider>
+      Meine heutigen Tasks:
+      <ul>
+        <li>React lernen</li>
+        <li>Mit dem Hund Gassi gehen</li>
+        <li>Obst essen</li>
+        <li>Brot kaufen</li>
+      </ul>
     </div>
   );
 }
