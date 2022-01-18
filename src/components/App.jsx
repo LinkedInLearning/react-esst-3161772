3c7ce51023bf95377c0318d3ca991f63033e758f
David Lorenz <info@activenode.de>
2022-01-18 17:56:51 +0100

    initial code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { BusinessProfile } from "./BusinessProfile";
 
 export function App() {
   return (
     <div>
       <h1>Contexts &amp; Theming</h1>
+
+      <BusinessProfile />
     </div>
   );
 }
---
+++ src/components/BusinessProfile.jsx
@@ -0,0 +1,14 @@
+export function BusinessProfile() {
+  // is person open-to-work ?
+
+  return (
+    <div className="profile">
+      <p>
+        <strong>🌟 David Lorenz</strong>
+      </p>
+      <p>
+        <small>Frontend Architect</small>
+      </p>
+    </div>
+  );
+}
