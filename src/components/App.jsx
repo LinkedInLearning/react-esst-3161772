b0d863d22b38ad9a8474bcecea1ec6a489282c0e
David Lorenz <info@activenode.de>
2022-01-18 20:48:28 +0100

    sample done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 export function App() {
+  const tasks = [
+    { id: 123, taskText: "React lernen 2" },
+    { id: 456, taskText: "Mit dem Hund Gassi gehen" },
+    { id: 789, taskText: "Obst essen" },
+    { id: 1011, taskText: "Brot kaufen" },
+  ];
+
   return (
     <div>
       <h1>Meine heutigen Tasks:</h1>
 
       <ul>
-        <li>React lernen</li>
-        <li>Mit dem Hund Gassi gehen</li>
-        <li>Obst essen</li>
-        <li>Brot kaufen</li>
+        {tasks.map((taskObj) => (
+          <li key={taskObj.id}>{taskObj.taskText}</li>
+        ))}
       </ul>
     </div>
   );
