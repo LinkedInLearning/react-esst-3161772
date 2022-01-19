8dccc2bd822d18a3dc76080ca32d305a5e82d872
David Lorenz <info@activenode.de>
2022-01-19 18:14:32 +0100

    sample scaled down

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,100 +1,16 @@
-import React, { useRef } from "react";
-import { Navigation } from "./Navigation";
+import React from "react";
 
 export function App() {
-  const refContainer = useRef(null);
-  const buttonClicked = () => {
-    // jump to nav
-    // nav.focus();
-    const nav = refContainer.current;
-
-    if (nav !== null) {
-      nav.focus();
-    }
-  };
-
   return (
     <>
       <header>
-        <Navigation ref={refContainer} />
+        <h1>Portals</h1>
         <img
           width="100%"
-          src="https://picsum.photos/id/251/700/300"
+          src="https://picsum.photos/id/255/700/300"
           alt=""
         />
       </header>
-
-      <main>
-        <article>
-          <h1>Beitrag 1</h1>
-          <p>
-            Vivamus sagittis lacus vel augue laoreet
-            rutrum faucibus dolor auctor. Maecenas sed
-            diam eget risus varius blandit sit amet non
-            magna. Maecenas sed diam eget risus varius
-            blandit sit amet non magna. Vivamus
-            sagittis lacus vel augue laoreet rutrum
-            faucibus dolor auctor. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris
-            condimentum nibh, ut fermentum massa justo
-            sit amet risus. Aenean lacinia bibendum
-            nulla sed consectetur.
-          </p>
-
-          <p>
-            Vivamus sagittis lacus vel augue laoreet
-            rutrum faucibus dolor auctor. Maecenas sed
-            diam eget risus varius blandit sit amet non
-            magna. Maecenas sed diam eget risus varius
-            blandit sit amet non magna. Vivamus
-            sagittis lacus vel augue laoreet rutrum
-            faucibus dolor auctor. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris
-            condimentum nibh, ut fermentum massa justo
-            sit amet risus. Aenean lacinia bibendum
-            nulla sed consectetur.
-          </p>
-        </article>
-        <article>
-          <h1>Beitrag 2</h1>
-          <p>
-            Vivamus sagittis lacus vel augue laoreet
-            rutrum faucibus dolor auctor. Maecenas sed
-            diam eget risus varius blandit sit amet non
-            magna. Maecenas sed diam eget risus varius
-            blandit sit amet non magna. Vivamus
-            sagittis lacus vel augue laoreet rutrum
-            faucibus dolor auctor. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris
-            condimentum nibh, ut fermentum massa justo
-            sit amet risus. Aenean lacinia bibendum
-            nulla sed consectetur.
-          </p>
-        </article>
-        <article>
-          <h1>Beitrag 3</h1>
-          <p>
-            Vivamus sagittis lacus vel augue laoreet
-            rutrum faucibus dolor auctor. Maecenas sed
-            diam eget risus varius blandit sit amet non
-            magna. Maecenas sed diam eget risus varius
-            blandit sit amet non magna. Vivamus
-            sagittis lacus vel augue laoreet rutrum
-            faucibus dolor auctor. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris
-            condimentum nibh, ut fermentum massa justo
-            sit amet risus. Aenean lacinia bibendum
-            nulla sed consectetur.
-          </p>
-        </article>
-      </main>
-
-      <footer>
-        <hr />
-        <button type="button" onClick={buttonClicked}>
-          Zur Navigation springen
-        </button>
-      </footer>
     </>
   );
 }
