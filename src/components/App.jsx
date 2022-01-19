a90745fc5c127fde1e0530d70ba7cb3f09f49772
David Lorenz <info@activenode.de>
2022-01-19 14:18:30 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,17 @@
 import React from "react";
 
-function MyComponent(props) {
-  return props.children;
-}
-
 export function App() {
-  const terms = [
-    {
-      term: "Firefox",
-      description: "Firefox is a Web Browser",
-    },
-    {
-      term: "Spaghetti",
-      description: "Spaghetti is Pasta",
-    },
-  ];
+  // LOGIC HERE:
+  // .....
+
+  // RENDER HERE:
   return (
     <>
-      <h1>Fragments</h1>
+      <h1>Kanye says</h1>
 
-      <dl>
-        {terms.map((termObj) => {
-          return (
-            <React.Fragment key={termObj.term}>
-              <dt>{termObj.term}</dt>
-              <dd>{termObj.description}</dd>
-            </React.Fragment>
-          );
-        })}
-      </dl>
-
-      <MyComponent>
-        <p>Hello Learning Cat!</p>
-        <p>Hello Learning Mouse!</p>
-      </MyComponent>
+      <ul>
+        <li></li>
+      </ul>
     </>
   );
 }
