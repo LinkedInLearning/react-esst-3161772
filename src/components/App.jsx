cc068eaa8adee9f0c1b4c68b0ba239f069cc0749
David Lorenz <info@activenode.de>
2022-01-21 12:08:52 +0100

    more prep fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Home } from "./Home";
 import { Photos } from "./Photos";
 import { SinglePhoto } from "./SinglePhoto";
@@ -18,8 +13,8 @@ export function App() {
         <h1>Routing 🔀</h1>
 
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/photos">Fotos</Link>
+          <a href="#">Home</a>
+          <a href="#">Fotos</a>
         </nav>
       </header>
 
