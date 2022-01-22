813cd9eb911fef558b265e5526fc2dd44acdc739
David Lorenz <info@activenode.de>
2022-01-22 16:42:07 +0100

    scaled down to minimum

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-import { Home } from "./Home";
-import { Resources } from "./Resources";
-import { Hero } from "./Hero";
 import "../styles/global.scss";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <Link to="/">Home</Link>{" "}
-          <Link to="/resources">Resources</Link>
-        </nav>
+    <>
+      <header>Hallo Header!</header>
 
-        <Hero />
-      </header>
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route
-            path="/resources"
-            element={<Resources />}
-          ></Route>
-        </Routes>
-      </main>
-    </BrowserRouter>
+      <main>Hallo Content</main>
+    </>
   );
 }
