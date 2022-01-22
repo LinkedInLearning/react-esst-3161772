29a40fc8a55df351d27becbea67a4369a8570b34
David Lorenz <info@activenode.de>
2022-01-22 13:15:21 +0100

    css stuff foundation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Home } from "./Home";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <>
+      <header>
+        <nav>
+          <a href="/">Home</a>
+          <a href="https://reactjs.org/">React Docs</a>
+        </nav>
+
+        <Hero />
+      </header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/resources"
+            element={"Resources"}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>React ist ein wirklich starkes Framework!</p>
+    </>
+  );
+}
