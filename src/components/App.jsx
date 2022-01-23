039eea6732392ff559efac7211ce632a4f89a7e2
David Lorenz <info@activenode.de>
2022-01-23 07:55:05 +0100

    memo cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "../styles/global.scss";
+import { MemoCard } from "./MemoCard";
 
 export function App() {
   return (
     <>
-      <header>Hallo Header!</header>
-
-      <main>Hallo Content</main>
+      <MemoCard
+        front="Was ist React?"
+        back="Ein universelles JavaScript Framework"
+      />
+      <MemoCard
+        front="Wer ist David Lorenz?"
+        back="Eine Person die gerne mit React arbeitet"
+      />
+      <MemoCard
+        front="Wo gibt es leckeres Eis?"
+        back="Einfach gefrorene Früchte in den Mixer! Ergo: Zuhause!"
+      />
     </>
   );
 }
---
+++ src/components/MemoCard.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import styles from "../styles/MemoCard.module.scss";
+
+export function MemoCard(props) {
+  const [showBack, setShowBack] = useState(false);
+  const { front, back } = props;
+
+  const toggle = () => setShowBack(!showBack);
+
+  return (
+    <div className={styles.card} onClick={toggle}>
+      <small>{showBack ? "Back" : "Front"}</small>
+
+      {!showBack && (
+        <strong className={styles.cardQuestion}>
+          {front}
+        </strong>
+      )}
+      {showBack && (
+        <p className={styles.cardAnswer}>{back}</p>
+      )}
+    </div>
+  );
+}
