ac9a93322f0de512cf1d5de8532e8b2772e66ff7
David Lorenz <info@activenode.de>
2022-01-23 15:20:15 +0100

    added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { MemoCard } from "./MemoCard";
 
 export function App() {
+  const foobar = "<em>Hallo!</em>";
   return (
     <>
+      <p
+        dangerouslySetInnerHTML={{ __html: foobar }}
+      ></p>
       <MemoCard
         front="Wie kann ich JSX Comments nutzen?"
         back="Indem JavaScript Kommentare genutzt werden :)"
       />
+      {/** hallo JSX comment! */}
       <MemoCard
-        front="Kann JSX innerHTML?"
-        back="Ja aber es ist gefährlich!"
+        front="Kann ich mit JSX HTML aus Variablen setzen?"
+        back="Ja, aber es ist gefährlich!"
       />
     </>
   );
