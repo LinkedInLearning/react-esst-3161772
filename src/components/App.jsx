126b35f5e55c62d476c00a5eb3e6d4bcaa9a30a3
David Lorenz <info@activenode.de>
2022-01-31 15:12:52 +0100

    preset done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 export function App() {
-  const [countdown, setCountdown] = useState(10);
-  useEffect(() => {
-    const ref = setTimeout(() => {
-      if (countdown > 0) {
-        setCountdown(countdown - 1);
-      }
-    }, 1000);
-
-    return () => clearTimeout(ref);
-  }, [countdown]);
+  const isRunning = false;
+  const timePlaceholder = "--:--:-- `--";
+  const startTime = new Date().getTime();
+  const currentTime = new Date().getTime();
+  const timeDelta = currentTime - startTime;
 
   return (
     <div>
-      <h1>Countdown</h1>
-      <p>{countdown}</p>
-      <progress value={countdown} min="0" max="10" />
+      <h1>Stoppuhr</h1>
+      <p>{!isRunning ? timePlaceholder : timeDelta}</p>
     </div>
   );
 }
