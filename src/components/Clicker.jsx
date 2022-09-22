4b3c423ebb022d2d5f54c68a45fbd6e7b6d74201
David Lorenz <david@wahnsinn.design>
2022-09-22 12:49:26 +0200

    more changes

--- src/components/Clicker.jsx
+++ src/components/Clicker.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 export function Clicker() {
   const [countClicks, setCountClicks] = useState(0);
   const [updateTime, setUpdateTime] = useState('bisher nie');
@@ -7,5 +8,10 @@ export function Clicker() {
   return <>
     <p>Anzahl Klicks = {countClicks}</p>
     <p>Zuletzt geklickt = {updateTime}</p>
+    <p>
+      <button type="button">
+        Klick mich
+      </button>
+    </p>
   </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './ClickerStyles.css';
 import { Clicker } from './components/Clicker';
 
 ReactDOM.render(
