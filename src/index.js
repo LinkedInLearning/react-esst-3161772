0904c6a020b216d4a80d9840cae6c785b0e7e8b9
David Lorenz <david@wahnsinn.design>
2022-09-22 12:17:39 +0200

    changed sample

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const myDiv = React.createElement('div', null, 'Inhalt des Divs');
-
-// ReactDOM.render(
-//   myDiv,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
   <React.StrictMode>
     <div>Hello!</div>
