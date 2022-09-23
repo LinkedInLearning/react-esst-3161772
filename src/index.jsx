7834b061abaf7548ed24ccea6660e96bfcb05db8
David Lorenz <david@wahnsinn.design>
2022-09-23 12:49:58 +0200

    updated sample

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./ClickerStyles.css";
 import { Clicker } from "./components/Clicker";
 
 const targetElement = document.getElementById("root");
+const root = ReactDOM.createRoot(targetElement);
 
-ReactDOM.render(<Clicker />, targetElement);
+root.render(<Clicker />);
